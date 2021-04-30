0cdc92cdcfc4920f7767b16320349be51b3c8e05
Joson5 <wangsanmu5@163.com>
2021-04-30 16:47:14 +0800

    init

---
+++ .umirc.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'dumi';
+
+export default defineConfig({
+  title: 'Joson',
+  mode: 'site',
+  locales: [
+    ['zh-CN', '中文'],
+    ['en-US', 'English'],
+  ],
+  // base: '/josonking.github.io/',
+  // publicPath: '/josonking.github.io/',
+  // more config: https://d.umijs.org/config
+});
